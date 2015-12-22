1f6bce92aa94981bf8f386cad587e47ea5a52af1
Nima Goniband <n.goniband@gmail.com>
2015-12-22 12:04:02 +0100

    Testing script and passing parameters

--- curler.bash
+++ curler.bash
@@ -1,4 +1,6 @@
-#!
+#!/bin/bash
 # This is the first script, in which we will curl the www.bth.se domain, 
 # and from there gather all the links. By using regex to find all the anchors 
 # in the html, and adding the links inside to the worklist (worklist.txt)
+
+curl $1 
