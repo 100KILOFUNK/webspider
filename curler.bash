e7f6406b95f15154cbd1e7c05228f4a59bda68a0
Nima Goniband <n.goniband@gmail.com>
2015-12-22 14:22:38 +0100

    The curler now only searches for and outputs www.bth.se domains

--- curler.bash
+++ curler.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
-# This is the first script, in which we will curl the www.bth.se domain, 
-# and from there gather all the links. By using regex to find all the anchors 
+# This is the first script, in which we will curl the www.bth.se webpage,
+# and from there gather all the links. By using regex to find all the anchors
 # in the html, and adding the links inside to the worklist (worklist.txt)
 
-curl $1 | sed -n 's/.*href="\([^"]*\).*/\1/p' >workinglist;
+curl $1 | sed -n 's/.*href="\([^"]*\).*/\1/p' | while read next
+do
+if echo $next | grep "bth.se"
+then echo $next >workinglist.txt 
+fi
+done
+
 
 exit
 
