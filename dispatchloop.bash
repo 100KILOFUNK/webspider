61fa632c4091db1ec229496254a385670950d630
Nima Goniband <n.goniband@gmail.com>
2015-12-22 14:43:59 +0100

    Update, no added functionality.

--- dispatchloop.bash
+++ dispatchloop.bash
@@ -1,3 +1,7 @@
-#!
+#!/bin/bash
 # This script shall work through the worklist and call the first script
 # curler.bash for each entry in the worklist. and write each sublink of the entry next to the entry
+
+echo "Not Yet Finished!"
+
+exit
--- grapher.bash
+++ grapher.bash
@@ -1,2 +1,6 @@
-#!
+#!/bin/bash
 # Convert the worklist file to a .dot file that we later can use to make a graph over the domain.
+
+echo "Not yet finished!"
+
+exit
