e56c3f7df83ce85d1d0ea0398ad7ba4ca759d6dd
Nima Goniband <n.goniband@gmail.com>
2015-12-29 12:06:54 +0100

    Information on how the script should work

--- dispatchloop.bash
+++ dispatchloop.bash
@@ -5,4 +5,13 @@
 #first run
 ./curler.bash www.bth.se
 
+
+cat list.txt | while read next
+do
+	# Here, we add the code which get the target information(not the source) from list.txt
+	# and then call the curler script with that information.
+done
+
+
+
 exit
