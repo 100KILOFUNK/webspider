e24ff38930ca6adf75037e7ad73f78a568c8e1a4
Nima Goniband <n.goniband@gmail.com>
2016-01-13 00:47:55 +0100

    dot -Tsvg grapher.dot -o graph.jpg

--- grapher.bash
+++ grapher.bash
@@ -1,6 +1,23 @@
 #!/bin/bash
 # Convert the worklist file to a .dot file that we later can use to make a graph over the domain.
 
-echo "Not yet finished!"
+echo "Creating graph.dot"
 
+# while loop which makes each line into the form 
+# sourcedomain+sourcepath -- targetdomain+targetpath;
+
+cat list.txt | while read next
+do
+	target=$(echo $next | awk '{print $3, $4}' | sed 's# -##')
+	source=$(echo $next | awk '{print $1, $2}' | sed 's# -##')
+	echo "	\""$source"\"" "->" "\""$target"\";" >>graph.dot
+#	sleep 3
+done
+
+
+#this is to be done last so we can make it into a jpg
+sed -i '1 i\digraph{' graph.dot
+echo "}" >>graph.dot
+sed -e '/" " -> " ";/d' graph.dot
+echo "Done!"
 exit
