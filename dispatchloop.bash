e32774de0259b7531a3befef6c4f6794700485d2
Nima Goniband <n.goniband@gmail.com>
2016-01-12 22:01:06 +0100

    small format changes

--- dispatchloop.bash
+++ dispatchloop.bash
@@ -8,32 +8,32 @@
 
 cat list.txt | while read next
 do
-	# kod. 
+ 
 	# do an if statement which check the first char in the column. if it is an /
 	# then it is local. Else use the targetdomain+targetpath in the loop
 	# initialize two variables in the beginning, the Domain and the path. 
 	# send them to the curler script at the end of the loop.
 
 
-if 	echo $next | awk '{print $3, $4}' | sed 's#-##' | awk '{print $1}' | cut -c 1 | grep "h" 
+if 	echo $next | awk '{print $3, $4}' | sed 's#-##' | awk '{print $1}' | cut -c 1 | grep -q "h" 
 then 
 	
 	domain=$(echo $next | awk '{print $3, $4}' | sed 's#-##' | awk '{print $1}')
 	path=$(echo $next | awk '{print $3, $4}' | sed 's#-##' | awk '{print $2}')
 	echo $domain
 	echo $path
-	./curler.bash $domain $path 
-elif    echo $next | awk '{print $3, $4}' | sed 's#-##' | awk '{print $1}' | cut -c 1 | grep "/"  
+ 
+elif    echo $next | awk '{print $3, $4}' | sed 's#-##' | awk '{print $1}' | cut -c 1 | grep -q  "/"  
 then
 	path=$(echo $next | awk '{print $3, $4}' | sed 's#-##' | awk '{print $1}')
 	domain=$(echo $next | awk '{print $1, $2}' | sed 's# -##')
 	echo $domain
 	echo $path
-	./curler.bash $domain $path
+
 fi
 
-#./curler.bash $domain $path
-sleep 2
+
+./curler.bash $domain $path
 done
 
 
